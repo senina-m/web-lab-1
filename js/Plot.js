620a249ee91c759f20dd65289cfb1b4691f73af8
Maria Senina <senina.m.m@yandex.ru>
2021-08-16 16:40:46 +0300

    drawing many plots fixed, init function to draw plot added

--- js/Plot.js
+++ js/Plot.js
@@ -2,32 +2,52 @@ const WIDTH = 500; //todo: рассчитывать изходя из разме
 const HEIGHT = 500;
 const X_CENTER = 0;
 const Y_CENTER = 0;
-const R = 2;
+const BACKGROUND_COLOR = '#fff';
+let CANVAS = null;
+const AXES_COLOR = '#000'
+const AREA_COLOR = '#1ff';
 let scale = 0.017;
 
 drawPlot = (pointsArray) => {
+
+
+    if (pointsArray === undefined) {
+        CANVAS = SVG()
+            .addTo('#plot')
+            .size(WIDTH, HEIGHT)
+            .viewbox(0, 0, WIDTH, HEIGHT);
+        //r by default
+        const defaultR = 2;
+        initPlot(defaultR);
+    } else {
+        drawPlotWithPoints(pointsArray);
+    }
+}
+
+initPlot = (r) => {
+    console.log('Init plot!');
+    CANVAS.rect(WIDTH, HEIGHT).fill(BACKGROUND_COLOR);
+    console.log("R value while init:" + r);
+    drawArea(r);
+    drawAxes();
+    drawAxesScaleLabels(r);
+}
+
+drawPlotWithPoints = (pointsArray) => {
     console.log('Ready to draw plot!')
-    document.getElementById('plot_block').style.visibility = "visible";
-    // document.getElementById('plot_image').remove();
-    const canvas = SVG()
-        .addTo('#plot')
-        .size(WIDTH, HEIGHT)
-        .viewbox(0, 0, WIDTH, HEIGHT);
-    const backgroundColor = '#fff';
-    const axesColor = '#000'
-    const areaColor = '#1ff';
-    canvas.rect(WIDTH, HEIGHT).fill(backgroundColor);
+    CANVAS.rect(WIDTH, HEIGHT).fill(BACKGROUND_COLOR);
+
     scale = countScale(pointsArray);
     let lastPoint = pointsArray[pointsArray.length - 1];
     const r = lastPoint.r;
     console.log('R = ' + r);
-    drawArea(canvas, r, backgroundColor, areaColor);
+    drawArea(r);
 
-    drawAxes(canvas, axesColor);
-    drawAxesScaleLabels(canvas, r, axesColor);
+    drawAxes();
+    drawAxesScaleLabels(r);
 
-    pointsArray.forEach(point => drawPoint(canvas, point.x, point.y, point.result, 5));
-    drawPoint(canvas, lastPoint.x, lastPoint.y, lastPoint.result, 10);
+    pointsArray.forEach(point => drawPoint(point.x, point.y, point.result, 5));
+    drawPoint(lastPoint.x, lastPoint.y, lastPoint.result, 10);
 }
 
 //todo: сделать так чтобы методы из этого файла были "приватные" -- либо переименовать, чтобы не путались
@@ -57,73 +77,74 @@ countScale = (pointsArray) => {
     return newScale;
 }
 
-drawAxes = (canvas, axesColor) => {
+drawAxes = () => {
     //todo: причесать метод, вынести дублирующийся код
     const arrowSize = 10
     //axis x
-    canvas.line(0, (HEIGHT / 2), WIDTH, (HEIGHT / 2)).stroke({width: 2, color: axesColor});
+    CANVAS.line(0, (HEIGHT / 2), WIDTH, (HEIGHT / 2)).stroke({width: 2, color: AXES_COLOR});
     //axis arrow
     const triangleX = (WIDTH - arrowSize) + ',' + (HEIGHT / 2 - arrowSize / 2) + ' ' +
         (WIDTH - arrowSize) + ',' + (HEIGHT / 2 + arrowSize / 2) + ' ' +
         (WIDTH) + ',' + (HEIGHT / 2)
     console.log('x arrow coordinates ' + triangleX)
-    canvas.polygon(triangleX).fill(axesColor)
-    canvas.text('x').font({
+    CANVAS.polygon(triangleX).fill(AXES_COLOR)
+    CANVAS.text('x').font({
         size: 16,
         family: 'Menlo, sans-serif',
         anchor: 'end',
-        fill: axesColor
+        fill: AXES_COLOR
     }).move(WIDTH - 2 * arrowSize, HEIGHT / 2 - 2 * arrowSize)
 
     //axis y
-    canvas.line(WIDTH / 2, 0, WIDTH / 2, HEIGHT).stroke({width: 2, color: axesColor});
+    CANVAS.line(WIDTH / 2, 0, WIDTH / 2, HEIGHT).stroke({width: 2, color: AXES_COLOR});
     //axis arrow
     const triangleY = (WIDTH / 2 - arrowSize / 2) + ',' + (arrowSize) + ' ' +
         (WIDTH / 2 + arrowSize / 2) + ',' + (arrowSize) + ' ' +
         (WIDTH / 2) + ',' + (0);
     console.log('y arrow coordinates ' + triangleY)
-    canvas.polygon(triangleY).fill(axesColor)
-    canvas.text('y').font({
+    CANVAS.polygon(triangleY).fill(AXES_COLOR)
+    CANVAS.text('y').font({
         size: 16,
         family: 'Menlo, sans-serif',
         anchor: 'end',
-        fill: axesColor
+        fill: AXES_COLOR
     }).move(WIDTH / 2 - 1.5 * arrowSize, 1.7 * arrowSize)
 }
 
-function drawScaleLabel(canvas, axesColor, xStart, xStop, yStart, yStop, labelX, labelY, label) {
-    // console.log('Label stroke input coordinates for ' + label + ': ' + xStart + ' ' + yStart + ' ' + xStop + ' ' + yStop)
-    // console.log('Label stroke coordinates ' + label + ': ' + convertX(xStart) + ' ' + convertY(yStart) + ' ' + convertX(xStop) + ' ' + convertY(yStop) + '\n');
-    canvas.line(convertX(xStart), convertY(yStart), convertX(xStop), convertY(yStop))
-        .stroke({width: 2, color: axesColor});
-    canvas.text(label).font({
+function drawScaleLabel(xStart, xStop, yStart, yStop, labelX, labelY, label) {
+    console.log('Label stroke input coordinates for ' + label + ': ' + xStart + ' ' + yStart + ' ' + xStop + ' ' + yStop)
+    console.log('Label stroke coordinates ' + label + ': ' + convertX(xStart) + ' ' + convertY(yStart) + ' ' + convertX(xStop) + ' ' + convertY(yStop) + '\n');
+    CANVAS.line(convertX(xStart), convertY(yStart), convertX(xStop), convertY(yStop))
+        .stroke({width: 2, color: AXES_COLOR});
+    CANVAS.text(label).font({
         size: 16,
         family: 'Menlo, sans-serif',
         anchor: 'end',
-        fill: axesColor
+        fill: AXES_COLOR
     }).move(convertX(labelX), convertY(labelY));
 }
 
-drawAxesScaleLabels = (canvas, r, axesColor) => { //todo: высчитывать координаты аккуратнее
+drawAxesScaleLabels = (r) => { //todo: высчитывать координаты аккуратнее
     console.log('Start drawing axes labels')
     const hatchLen = 0.1;
+    console.log("R value while drawing labels: " + r);
     //x axis labels
-    drawScaleLabel(canvas, axesColor, -r, -r, hatchLen, -hatchLen, -r, -2 * hatchLen, "-2R");
-    drawScaleLabel(canvas, axesColor, -r / 2, -r / 2, hatchLen, -hatchLen, -r / 2, -2 * hatchLen, "-R");
-    drawScaleLabel(canvas, axesColor, r / 2, r / 2, hatchLen, -hatchLen, r / 2, -2 * hatchLen, "R");
-    drawScaleLabel(canvas, axesColor, r, r, hatchLen, -hatchLen, r, -2 * hatchLen, "2R");
+    drawScaleLabel(-r, -r, hatchLen, -hatchLen, -r, -2 * hatchLen, "-R");
+    drawScaleLabel(-r / 2, -r / 2, hatchLen, -hatchLen, -r / 2, -2 * hatchLen, "-R/2");
+    drawScaleLabel(r / 2, r / 2, hatchLen, -hatchLen, r / 2, -2 * hatchLen, "R/2");
+    drawScaleLabel(r, r, hatchLen, -hatchLen, r, -2 * hatchLen, "R");
 
     //y axis labels
-    drawScaleLabel(canvas, axesColor, hatchLen, -hatchLen, -r, -r, -4 * hatchLen, -r, "-2R");
-    drawScaleLabel(canvas, axesColor, hatchLen, -hatchLen, -r / 2, -r / 2, -4 * hatchLen, -r / 2, "-R");
-    drawScaleLabel(canvas, axesColor, hatchLen, -hatchLen, r / 2, r / 2, -4 * hatchLen, r / 2, "R");
-    drawScaleLabel(canvas, axesColor, hatchLen, -hatchLen, r, r, -4 * hatchLen, r, "2R");
+    drawScaleLabel( hatchLen, -hatchLen, -r, -r, -4 * hatchLen, -r, "-R");
+    drawScaleLabel( hatchLen, -hatchLen, -r / 2, -r / 2, -4 * hatchLen, -r / 2, "-R/2");
+    drawScaleLabel( hatchLen, -hatchLen, r / 2, r / 2, -4 * hatchLen, r / 2, "R/2");
+    drawScaleLabel( hatchLen, -hatchLen, r, r, -4 * hatchLen, r, "R");
 }
 
 
-drawArea = (canvas, r, backgroundColor, areaColor) => {
+drawArea = (r) => {
     //here diameter needed
-    canvas.circle(r / scale).fill(areaColor).move(convertX(-r / 2), convertY(r / 2))
+    CANVAS.circle(r / scale).fill(AREA_COLOR).move(convertX(-r / 2), convertY(r / 2))
     const fillUnusedCircle = (convertX(0)) + ',' + (convertY(0)) + ' ' +
         (convertX(-r / 2)) + ',' + (convertY(0)) + ' ' +
         (convertX(-r / 2)) + ',' + (convertY(r / 2)) + ' ' +
@@ -131,7 +152,7 @@ drawArea = (canvas, r, backgroundColor, areaColor) => {
         (convertX(r / 2)) + ',' + (convertY(-r / 2)) + ' ' +
         (convertX(0)) + ',' + (convertY(-r / 2))
 
-    canvas.polygon(fillUnusedCircle).fill(backgroundColor)
+    CANVAS.polygon(fillUnusedCircle).fill(BACKGROUND_COLOR)
     const area = (convertX(0)) + ',' + (convertY(0)) + ' ' +
         (convertX(-r / 2)) + ',' + (convertY(0)) + ' ' +
         (convertX(0)) + ',' + (convertY(r / 2)) + ' ' +
@@ -140,7 +161,7 @@ drawArea = (canvas, r, backgroundColor, areaColor) => {
         (convertX(r / 2)) + ',' + (convertY(-r)) + ' ' +
         (convertX(0)) + ',' + (convertY(-r))
     console.log('area coordinates ' + area)
-    canvas.polygon(area).fill(areaColor)
+    CANVAS.polygon(area).fill(AREA_COLOR)
 }
 
 countPointLocation = (coords) => {
@@ -152,9 +173,9 @@ countPointLocation = (coords) => {
         || (x + r / 2 >= y && y >= 0 && x >= 0));
 }
 
-drawPoint = (canvas, x, y, result, pointScale) => {
+drawPoint = (x, y, result, pointScale) => {
     let color = result === 'true' ? '#0f0' : '#f00';
-    canvas.circle(pointScale).fill(color).move(convertX(x), convertY(y))
+    CANVAS.circle(pointScale).fill(color).move(convertX(x), convertY(y))
     // console.log('x:' + x + ', y:' + y)
     // console.log('scale ' + pointScale + ', x:' + convertX(x) + ', y:' + convertY(y))
 }
