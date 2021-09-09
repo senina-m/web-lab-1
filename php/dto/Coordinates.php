075fd503ff59386b13fcc6e9d9b687568ec6b053
Maria Senina <senina.m.m@yandex.ru>
2021-09-09 10:52:11 +0300

    fix data validation on server side -- now it's an exceptional situation if user entered value that could be number, but it isn't like "2a"

--- php/dto/Coordinates.php
+++ php/dto/Coordinates.php
@@ -11,17 +11,17 @@ class Coordinates
     {
 //        var_dump($coordinates);
         $this->x = (int) ($coordinates->{"x"});
-        if ($this->x > 4 or $this->x < -4) {
+        if ($this->x > 4 or $this->x < -4 or !(is_numeric($coordinates->{"x"})) ) {
             throw new Incorrect_input_data_exception("X coordinate isn't in -4..4");
         }
 
         $this->y = (double)($coordinates->{"y"});
-        if ($this->y > 3 or $this->y < -5) {
+        if ($this->y > 3 or $this->y < -5 or !(is_numeric($coordinates->{"y"}))) {
             throw new Incorrect_input_data_exception("Y coordinate isn't in -5..3");
         }
 
         $this->r = (double)($coordinates->{"r"});
-        if ($this->r > 5 or $this->r < 2) {
+        if ($this->r > 5 or $this->r < 2 or !(is_numeric($coordinates->{"r"}))) {
             throw new Incorrect_input_data_exception("R isn't in -2..5");
         }
     }
